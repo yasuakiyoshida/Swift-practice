559467dff128c66173541f1c460401198a4f7381
Yasuaki Yoshida <yasu5293koh@gmail.com>
2021-03-14 11:19:20 +0900

    SampleQuiz Part6

--- SampleQuiz/SampleQuiz/QuizViewController.swift
+++ SampleQuiz/SampleQuiz/QuizViewController.swift
@@ -14,10 +14,12 @@ class QuizViewController: UIViewController {
     @IBOutlet weak var answerButton2: UIButton!
     @IBOutlet weak var answerButton3: UIButton!
     @IBOutlet weak var answerButton4: UIButton!
+    @IBOutlet weak var judgeImageView: UIImageView!
     
     var csvArray: [String] = []
     var quizArray: [String] = []
     var quizCount = 0
+    var correctCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,8 +39,49 @@ class QuizViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let scoreVC = segue.destination as! ScoreViewController
+        scoreVC.correct = correctCount
+    }
+    
     @IBAction func btnAction(sender: UIButton) {
-        print(sender.tag)
+        if sender.tag == Int(quizArray[1]) {
+            print("正解")
+            correctCount += 1
+            judgeImageView.image = UIImage(named:  "correct")
+        } else {
+            print("不正解")
+            judgeImageView.image = UIImage(named:  "incorrect")
+        }
+        print("スコア:\(correctCount)")
+        judgeImageView.isHidden = false
+        answerButton1.isEnabled = false
+        answerButton2.isEnabled = false
+        answerButton3.isEnabled = false
+        answerButton4.isEnabled = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.judgeImageView.isHidden = true
+            self.answerButton1.isEnabled = true
+            self.answerButton2.isEnabled = true
+            self.answerButton3.isEnabled = true
+            self.answerButton4.isEnabled = true
+            self.nextQuiz()
+        }
+    }
+    
+    func nextQuiz() {
+        quizCount += 1
+        if quizCount < csvArray.count {
+            quizArray = csvArray[quizCount].components(separatedBy: ",")
+            quizNumberLabel.text = "第\(quizCount + 1)問"
+            quizTextView.text = quizArray[0]
+            answerButton1.setTitle(quizArray[2], for: .normal)
+            answerButton2.setTitle(quizArray[3], for: .normal)
+            answerButton3.setTitle(quizArray[4], for: .normal)
+            answerButton4.setTitle(quizArray[5], for: .normal)
+        } else {
+            performSegue(withIdentifier: "toScoreVC", sender: nil)
+        }
     }
     
     func loadCSV(fileName: String) -> [String] {
--- SampleQuiz/SampleQuiz/ScoreViewController.swift
+++ SampleQuiz/SampleQuiz/ScoreViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class ScoreViewController: UIViewController {
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var correct = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scoreLabel.text = "\(correct)問正解！"
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func toTopButtonAction(_ sender: Any) {
+        self.presentingViewController?.presentingViewController?.dismiss(animated: true)
     }
     
 
